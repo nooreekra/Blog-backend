481097fdbfe8f86aaa61116aae55bfd1dc0c755e
nooreekra <rakhymzhan.ff@gmail.com>
2022-06-29 01:21:02 +0600

    add some code for db

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 import mongoose from "mongoose";
 import {validationResult} from 'express-validator';
 
@@ -8,7 +9,7 @@ import {registerValidation} from './validtaions/auth.js';
 import UserModel from './models/User.js';
 
 mongoose
-    .connect('mongodb+srv://root:wwwwww@cluster0.bvngfwt.mongodb.net/?retryWrites=true&w=majority')
+    .connect('mongodb+srv://root:wwwwww@cluster0.bvngfwt.mongodb.net/blog?retryWrites=true&w=majority')
     .then(() => console.log('DB ok'))
     .catch((err) => console.log('DB error', err));
 const app = express();
@@ -16,25 +17,41 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) =>{
-    const errors = validationResult(req);
-    if(!errors.isEmpty()) {
-        return res.status(400).json(errors.array());
-    }
+    try {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json(errors.array());
+        }
 
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const passwordHash = await bcrypt.hash(password, salt);
+        const password = req.body.password;
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(password, salt);
 
-    const doc = new UserModel({
-        email: req.body.email,
-        fullName: req.body.fullName,
-        avatarUrl: req.body.avatarUrl,
-        passwordHash,
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash,
+        });
+
+        const user = await doc.save();
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'secret123', {
+            expiresIn: '30d',
+        });
+
+        res.json({
+            ...user._doc,
+            token,
     });
-
-    const user = await doc.save();
-
-    res.json(user);
+    } catch (err){
+        console.log(err);
+        res.status(500).json({
+            message: 'Не удалось зарегистрироваться',
+        });
+    }
 
 });
 
