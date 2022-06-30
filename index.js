5fbfd2d992ef0f21fa09ced14bd1309d09880e71
nooreekra <rakhymzhan.ff@gmail.com>
2022-06-30 21:44:13 +0600

    checking backend

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ import express from 'express';
 import fs from 'fs';
 import multer from 'multer';
 import cors from 'cors';
-import mongoose from "mongoose";
 
-import {registerValidation, loginValidation, postCreateValidation} from './validations.js';
+import mongoose from 'mongoose';
 
-import { checkAuth, handleValidationErrors } from './utils/index.js';
+import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
+
+import { handleValidationErrors, checkAuth } from './utils/index.js';
 
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-    .connect(process.env.MONGODB_URI)
-    .then(() => console.log('DB ok'))
-    .catch((err) => console.log('DB error', err));
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
+
 const app = express();
 
 const storage = multer.diskStorage({
-    destination: (_, __, cb) => {
-        if(!fs.existsSync('uploads')) {
-            fs.mkdirSync('uploads');
-        }
-        cb(null, 'uploads');
-    },
-    filename: (_, file, cb) => {
-        cb(null, file.originalname);
-    },
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-const upload = multer({storage});
+const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors());
@@ -39,9 +41,9 @@ app.post('/auth/register', registerValidation, handleValidationErrors, UserContr
 app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-        url: `/uploads/${req.file.originalname}`,
-    });
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
 });
 
 app.get('/tags', PostController.getLastTags);
@@ -52,16 +54,17 @@ app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
 app.patch(
-    '/posts/:id',
-    checkAuth,
-    postCreateValidation,
-    handleValidationErrors,
-    PostController.update);
-
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
 
 app.listen(process.env.PORT || 4444, (err) => {
-    if (err) {
-        return console.log(err)
-    }
-    console.log('Server OK');
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('Server OK');
 });
